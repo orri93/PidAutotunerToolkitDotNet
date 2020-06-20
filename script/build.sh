4dda5d1ce328e1b840622088deb8185b62133afa
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2020-06-20 12:22:38 +0200

    Update NIX build script

--- script/build.sh
+++ script/build.sh
@@ -10,27 +10,29 @@ GOS_QML_PLUGIN=Pid.Toolkit
 
 # Resolve the Current Script Directory
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-SCRIPT_SOURCE="${BASH_SCRIPT_SOURCE[0]}"
-while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SCRIPT_SOURCE until the file is no longer a symlink
-  SCRIPT_TARGET="$(readlink "$SCRIPT_SOURCE")"
-  if [[ $SCRIPT_TARGET == /* ]]; then
-    echo "SCRIPT_SOURCE '$SCRIPT_SOURCE' is an absolute symlink to '$SCRIPT_TARGET'"
-    SCRIPT_SOURCE="$SCRIPT_TARGET"
+#!/bin/bash
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE="$TARGET"
   else
-    SCRIPT_DIR="$( dirname "$SCRIPT_SOURCE" )"
-    echo "SCRIPT_SOURCE '$SCRIPT_SOURCE' is a relative symlink to '$SCRIPT_TARGET' (relative to '$SCRIPT_DIR')"
-    SCRIPT_SOURCE="$SCRIPT_DIR/$SCRIPT_TARGET" # if $SCRIPT_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    DIR="$( dirname "$SOURCE" )"
+    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
   fi
 done
-echo "SCRIPT_SOURCE is '$SCRIPT_SOURCE'"
-RSCRIPT_DIR="$( dirname "$SCRIPT_SOURCE" )"
-SCRIPT_DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
-if [ "$SCRIPT_DIR" != "$RSCRIPT_DIR" ]; then
-  echo "SCRIPT_DIR '$RSCRIPT_DIR' resolves to '$SCRIPT_DIR'"
+echo "SOURCE is '$SOURCE'"
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+if [ "$DIR" != "$RDIR" ]; then
+  echo "DIR '$RDIR' resolves to '$DIR'"
 fi
-echo "SCRIPT_DIR is '$SCRIPT_DIR'"
+echo "DIR is '$DIR'"
 
-GOS_ROOT_DIR=$SCRIPT_DIR
+GOS_ROOT_DIR=DIR
 
 silent_pushd () {
     command pushd "$@" > /dev/null
